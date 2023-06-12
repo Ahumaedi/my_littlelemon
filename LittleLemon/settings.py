aab327365ef9c9d4fc7b7d11e44f22ae9a73e523
Ahumaedi <ahmadhumaedi0@gmail.com>
2023-06-12 22:26:16 +0700

    Set Registration

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Restaurant',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+    # 'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +135,16 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        # "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+
+DJOSER = {"USER_ID_FIELD":"username"}
